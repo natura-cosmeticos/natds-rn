b896bcef5502347accd3af12b1389a2e6fbbfc5b
Pamela Souza <pamela.p.de.souza@accenture.com>
2019-04-16 11:11:31 +0000

    Adjusting UI on Components already used
    * Merged in create-assets-folder (pull request #4)
    
        Create assets folder
    
        * include fonts, icons and theme folder to assets
    
        * link fonts to native
    
        * link fonts to native
    
        Approved-by: Michelle Araujo <michelle_araujo@mckinsey.com>
        Approved-by: Pamela Souza <pamela.p.de.souza@accenture.com>
    
    * Merged in feat/natbutton (pull request #6)
    
        Feat/natbutton
    
        * migraçao do componente NatButton
    
        * include fonts, icons and theme folder to assets
    
        * Button Style finalization
    
        * export NatButton from library
    
        * link fonts to native
    
        * Add icon on Button type card
    
        * fix font export
    
        * add tests for button type card
    
        Approved-by: Pamela Souza <pamela.p.de.souza@accenture.com>
        Approved-by: Natalia Braz <natalia.braz@accenture.com>
    
    * feat/header-migration
    
        Migration header component from App Consultoria 5.0 to natura-design-system
    
    * chore/update-path-name-folders-to-assets
    
    * chore/header-component
    
    * Merged in feat/navgriditem (pull request #9)
    
        Feat/navgriditem
    
        * header component snapshot update
    
        * refactoring and migration of the NatGridItem component
    
        Approved-by: Marcus Ortense <marcus.ortense@accenture.com>
        Approved-by: Natalia Braz <natalia.braz@accenture.com>
    
    * * corrected snapshot and adjustmented lint
    
    * refactoring spacing
    
    Approved-by: Natalia Braz <natalia.braz@accenture.com>
    Approved-by: Marcus Ortense <marcus.ortense@accenture.com>

--- App.js
+++ App.js
@@ -3,6 +3,6 @@ import { View, Text } from 'react-native';
 
 export const App = () => (
   <View style={{ height: '100%', alignItems: 'center', justifyContent: 'center' }}>
-     <Text>You can test your component here</Text>
+    <Text>You can test your component here</Text>
   </View>
 );
