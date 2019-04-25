a96cfffba422657e27f08d51bc7d9531dc392010
Pamela Souza <pamela.p.de.souza@accenture.com>
2019-04-25 11:53:59 +0000

    Merged in feat/tipography (pull request #17)
    Creation of typography assets
    
    * create typography
    
    * structuring and testing of assets
    
    * creation of typography assets
    
    Approved-by: Michelle Araujo <michelle_araujo@mckinsey.com>
    Approved-by: Natalia Braz <natalia.braz@accenture.com>

--- App.js
+++ App.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import { View, Text } from 'react-native';
+import { typography } from './lib/assets/theme/typography/typography.js';
 
 export const App = () => (
   <View style={{ height: '100%', alignItems: 'center', justifyContent: 'center' }}>
-    <Text>You can test your component here</Text>
+    <Text style={[typography.display_small]}>You can test your component here</Text>
   </View>
 );
