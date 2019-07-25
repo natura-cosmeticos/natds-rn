7b4d9862976820dc9a079652a5589dfd86800d86
Pamela Souza <pamela.p.de.souza@accenture.com>
2019-07-25 13:32:20 +0000

    Merged in nds-upgrade (pull request #35)
    Upgrade version 0.59.10
    
    Approved-by: Matheus Misumi <matheus.misumi@accenture.com>
    Approved-by: Natalia Braz <natalia.braz@accenture.com>

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ import { View, Text } from 'react-native';
 import { body } from './lib/assets/theme/typography/typography';
 
 export const App = () => (
-  <View style={{ height: '100%', alignItems: 'center', justifyContent: 'center' }}>
+  <View>
     <Text style={[body.Small]}>You can test your component here</Text>
   </View>
 );
