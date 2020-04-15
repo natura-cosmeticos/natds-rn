07d010151edd82fac4decf245527b31b85c9fe39
Ariel Flor <arielwb@gmail.com>
2020-04-15 00:28:26 -0300

    docs: test some storybook configs

--- storybook-web/main.js
+++ storybook-web/main.js
@@ -12,8 +12,28 @@ module.exports = {
       'react-native-svg': 'react-native-svg/lib/commonjs/ReactNativeSVG.web',
       // ...
     };
+    // config.module.rules.push({
+    //   test: /\.(gif|jpe?g|png|svg)$/,
+    //   use: {
+    //     loader: 'url-loader',
+    //     options: {
+    //       name: '[name].[ext]',
+    //     },
+    //   },
+    // });
     // mutate babel-loader
     config.module.rules[0].use[0].options.plugins.push(['react-native-web', { commonjs: true }]);
+    // config.module.rules[3] = {
+    //   test: /\.(gif|jpe?g|png|svg)$/,
+    //   use: {
+    //     loader: 'url-loader',
+    //     options: {
+    //       name: '[name].[ext]',
+    //     },
+    //   },
+    // }
+
+    // config.resolve.extensions.concat(['.web.js', '.js', '.json', '.web.jsx', '.jsx']);
 
     // console.dir(config, { depth: null });
     return config;
