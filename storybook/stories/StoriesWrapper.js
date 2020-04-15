25620f976cdbdd9afba1142f5a7498366f5e4721
Ariel Flor <arielwb@gmail.com>
2020-04-15 16:38:53 -0300

    docs: add theme section

--- storybook/stories/StoriesWrapper.js
+++ storybook/stories/StoriesWrapper.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { themes } from '@naturacosmeticos/natds-styles';
+import { ThemeProvider } from 'styled-components';
 import { ScrollView, SafeAreaView, Dimensions } from 'react-native';
 import { NatContainer, Button } from '../../lib';
 
@@ -14,19 +15,22 @@ export const StoriesWrapper = ({ story }) => {
   const [isLight, changeTheme] = useState(true);
 
   const mode = isLight ? 'light' : 'dark';
+  const theme = themes.natura[mode];
 
   return (
     <SafeAreaView style={styles.defaultScreen}>
-        <Button
-            type="primary"
-            label="Change Theme"
-            onPress={() => changeTheme(!isLight)}
-          />
-        <ScrollView>
-      <NatContainer>
-        {story && story({ light: isLight, theme: themes.natura[mode] })}
-      </NatContainer>
-        </ScrollView>
+      <Button
+          type="primary"
+          label="Change Theme"
+          onPress={() => changeTheme(!isLight)}
+        />
+      <ScrollView>
+        <ThemeProvider theme={theme}>
+          <NatContainer>
+            {story && story({ light: isLight, theme: themes.natura[mode] })}
+          </NatContainer>
+        </ThemeProvider>
+      </ScrollView>
     </SafeAreaView>
   );
 };
