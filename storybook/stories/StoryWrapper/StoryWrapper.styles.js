f3c0f7ea8faabbfdd3206e906637e2aadf8568db
Ariel Flor <arielwb@gmail.com>
2020-04-16 20:27:42 -0300

    docs: add theme selector header

---
+++ storybook/stories/StoryWrapper/StoryWrapper.styles.js
@@ -0,0 +1,55 @@
+import styled from 'styled-components/native';
+
+export const ContainerWithTheme = styled.View`
+  background-color: ${({ theme }) => theme.palette.background.default};
+`;
+
+export const Container = styled(ContainerWithTheme)`
+  align-items: center;
+  flex-direction: row;
+  justify-content: space-between;
+  padding: 10px;
+`;
+
+const addBorder = ({ isLast }) => {
+  if (isLast) return '';
+
+  return `
+      border-bottom-color: #eaeaea;
+      border-bottom-width: 1px;
+    `;
+};
+
+export const SwitchContainer = styled(Container)`
+  ${addBorder}
+`;
+
+export const ModalContainer = styled(ContainerWithTheme)`
+  flex: 1;
+  align-items: flex-start;
+  padding-top: 60px;
+  padding-left: 20px;
+  padding-right: 20px;
+`;
+
+export const Button = styled.TouchableHighlight`
+  border-radius: 20px;
+  border-color: ${({ theme }) => theme.palette.primary.main}; 
+  border-width: 1px;
+  padding: 10px;
+  elevation: 2;
+`;
+
+export const BlockButton = styled(Button)`
+  align-self: stretch;
+`;
+
+export const TextWithTheme = styled.Text`
+  color: ${({ theme }) => theme.palette.text.primary};
+`;
+
+export const Title = styled(TextWithTheme)`
+  margin-bottom: 15px;
+  font-size: 30px;
+  font-weight: 100;
+`;
