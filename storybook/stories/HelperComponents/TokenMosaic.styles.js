09d43f9842d92bd331147470a3290af88d111f3c
Ariel Flor <arielwb@gmail.com>
2020-04-17 19:14:35 -0300

    docs: add new colors section

---
+++ storybook/stories/HelperComponents/TokenMosaic.styles.js
@@ -0,0 +1,38 @@
+import styled from 'styled-components/native';
+import { TextWithTheme } from './ThemeHelper.styles';
+
+export const TokenContainer = styled.View`
+  flex-wrap: wrap;
+  flex-direction: row;
+  justify-content: space-between;
+  margin-left: -5px;
+  margin-right: -5px;
+`;
+
+export const TokenColorContainer = styled.View`
+  flex-basis: 50%;
+  padding: 5px;
+  margin-top: 5px;
+  margin-bottom: 5px;
+
+`;
+
+export const ActualColor = styled.View`
+  height: 50px;
+  margin-bottom: 5px;
+  border: solid 1px #eaeaea;
+`;
+
+export const ColorName = styled(TextWithTheme)`
+  font-size: 11px;
+`;
+
+export const CategoryTitle = styled(TextWithTheme)`
+  font-size: 20px;
+  font-weight: 100;
+`;
+
+export const CategoryWrapper = styled.View`
+  margin-top: 20px;
+  margin-bottom: 20px;
+`;
