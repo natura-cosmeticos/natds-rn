d82a2d1ae2e9aa28c9c583caae2a2a2ef5484f97
Ariel Flor <arielwb@gmail.com>
2020-04-17 22:06:24 -0300

    chore: fix lint

--- storybook/stories/StoryTokenColors/Colors.js
+++ storybook/stories/StoryTokenColors/Colors.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Text } from 'react-native'
+import { Text } from 'react-native';
 import { tokens } from '@naturacosmeticos/natds-styles';
 import { TokenMosaicWithCategories, TokenColor } from '../HelperComponents/TokenMosaic';
 
