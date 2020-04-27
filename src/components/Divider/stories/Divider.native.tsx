84d8bfe8f0a64214149b0bfd732b01a36ab44f8b
Ariel Flor <arielwb@gmail.com>
2020-04-27 12:56:26 -0300

    chore: fix native stories loading

--- src/components/Divider/stories/Divider.native.tsx
+++ src/components/Divider/stories/Divider.native.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react-native';
+import { Divider } from '../Divider';
 import { fullWidth, inset, middle } from './Divider.stories';
 
 storiesOf('Divider')
   .add('full width', fullWidth)
-  .add('inset', inset)
-  .add('middle', middle);
+  .add('inset ', inset)
+  .add('middle ', middle)
--- storybook/storyLoader.ts
+++ storybook/storyLoader.ts
@@ -4,11 +4,11 @@
 // https://github.com/elderfo/react-native-storybook-loader.git
 
 function loadStories() {
-	require('../src/components/Divider/stories/Divider.stories.native');
+	require('../src/components/Divider/stories/Divider.native');
 }
 
 const stories = [
-	'../src/components/Divider/stories/Divider.stories.native'
+	'../src/components/Divider/stories/Divider.native'
 ];
 
 module.exports = {
