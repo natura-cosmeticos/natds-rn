a18a469f9ec3971a28a2da0115fe5fd14bc1201a
Ariel Flor <arielwb@gmail.com>
2020-04-29 11:48:31 -0300

    chore: add pre-release initial config
    DSY-670

---
+++ bin/pre-release/pre-release.js
@@ -0,0 +1,14 @@
+#!/usr/bin/env node
+
+const read = require('@commitlint/read');
+const shell = require('shelljs');
+
+const init = async () => {
+  const commits = await read({ from: 'HEAD~2' });
+  const issueNumber = commits.find(issue => issue.match(/(DSY-\d+)/));
+  const preReleaseName = issueNumber || 'alpha';
+
+  shell.exec(`yarn release --prerelease ${preReleaseName}`);
+};
+
+init();
