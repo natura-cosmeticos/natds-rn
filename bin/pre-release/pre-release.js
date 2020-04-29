ec458d182c70cb2615ae98669fd4fa9556454557
Ariel Flor <arielwb@gmail.com>
2020-04-29 12:40:46 -0300

    chore: add push tags and publish
    DSY-670

--- bin/pre-release/pre-release.js
+++ bin/pre-release/pre-release.js
@@ -3,12 +3,22 @@
 const read = require('@commitlint/read');
 const shell = require('shelljs');
 
+const npmToken = process.env.NPM_TOKEN;
+
+if (!npmToken) {
+  console.log('NPM_TOKEN not found.');
+  process.exit(1);
+}
+
 const init = async () => {
   const commits = await read({ from: 'HEAD~2' });
   const issueNumber = commits.find(issue => issue.match(/(DSY-\d+)/g));
   const preReleaseName = issueNumber.match(/(DSY-\d+)/g)[0] || 'alpha';
 
   shell.exec(`yarn release --prerelease ${preReleaseName}`);
+  // shell.exec('git push --follow-tags');
+  shell.exec(`npm config set "//registry.npmjs.org/:_authToken" "${npmToken}"`);
+  shell.exec(`npm publish --tag ${preReleaseName}`);
 };
 
 init();
