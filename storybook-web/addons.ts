c7e3d987fb6e659f3e53eb0ee29610466fb24649
Ariel Flor <arielwb@gmail.com>
2020-04-29 19:52:54 -0300

    chore: remove unused addons and upgrade tokens

--- storybook-web/addons.ts
+++ storybook-web/addons.ts
@@ -5,4 +5,3 @@ import '@storybook/addon-viewport/register';
 import '@storybook/addon-knobs/register';
 import 'storybook-addon-designs/register';
 import './addons/theme/register';
-import './addons/versions/register';
