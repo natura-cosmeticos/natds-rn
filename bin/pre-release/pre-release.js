a683f08f5aac6d49ee2fdb2ec306db6ac4e94200
Ariel Flor <arielwb@gmail.com>
2020-04-29 11:54:33 -0300

    chore: add standart version
    DSY-670

--- bin/pre-release/pre-release.js
+++ bin/pre-release/pre-release.js
@@ -5,8 +5,8 @@ const shell = require('shelljs');
 
 const init = async () => {
   const commits = await read({ from: 'HEAD~2' });
-  const issueNumber = commits.find(issue => issue.match(/(DSY-\d+)/));
-  const preReleaseName = issueNumber || 'alpha';
+  const issueNumber = commits.find(issue => issue.match(/(DSY-\d+)/g));
+  const preReleaseName = issueNumber.match(/(DSY-\d+)/g)[0] || 'alpha';
 
   shell.exec(`yarn release --prerelease ${preReleaseName}`);
 };
