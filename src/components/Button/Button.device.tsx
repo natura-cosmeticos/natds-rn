fc00d950d6932d681ff19e1872853391b6503b90
Ariel Flor <arielwb@gmail.com>
2020-05-08 20:09:55 -0300

    chore(button): add kobs
    DSY-619

--- src/components/Button/Button.device.tsx
+++ src/components/Button/Button.device.tsx
@@ -1,10 +1,11 @@
 import { storiesOf } from '@storybook/react-native';
 import {
-  contained, outlined, text, all,
+  contained, outlined, text, all, interactive,
 } from './Button.stories';
 
 storiesOf('Button', module)
   .add('all', all)
   .add('contained', contained)
   .add('outlined', outlined)
-  .add('text', text);
+  .add('text', text)
+  .add('interactive', interactive);
