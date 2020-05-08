4c50f46dbe6bb165d2e96ba97f37d89e42d51e79
Ariel Flor <arielwb@gmail.com>
2020-05-08 19:40:13 -0300

    chore: add on device knobs support and examples
    DSY-670

--- storybook/config.tsx
+++ storybook/config.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { configure, addDecorator } from '@storybook/react-native';
+import { withKnobs } from '@storybook/addon-knobs';
 // @ts-ignore
 import { StoriesWrapper } from '../src/common/StoryWrapper';
 import { loadStories } from './storyLoader';
 
 console.disableYellowBox = true; // eslint-disable-line no-console
 
+addDecorator(withKnobs);
 addDecorator(story => <StoriesWrapper story={story} />);
 
 configure(() => {
