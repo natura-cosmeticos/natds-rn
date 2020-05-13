89730816f38f36ff7a84f541359eb958bf4a7376
Ariel Flor <arielwb@gmail.com>
2020-05-13 19:55:59 -0300

    chore: fix lib build
    DSY-793

--- src/common/themeSelectors/sizes/sizes.ts
+++ src/common/themeSelectors/sizes/sizes.ts
@@ -1,10 +1,20 @@
+
 import { Theme } from '..';
+interface ButtonProperties {
+  paddingTop: number;
+  paddingRight: number;
+  paddingBottom: number;
+  paddingLeft: number;
+  minHeight: number;
+}
 
 const getButtonSizes = (theme: Theme) => theme.buttonSizes;
 
 export type Size = 'large' | 'medium' | 'small'
 
-export const getButtonPropsBySize = (theme: Theme, size: Size) => getButtonSizes(theme)[size];
+export const getButtonPropsBySize = (
+  theme: Theme, size: Size,
+): ButtonProperties => getButtonSizes(theme)[size];
 
 const getRadiusSizes = (theme: Theme) => theme.radius;
 
