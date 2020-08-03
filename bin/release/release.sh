823aa20c51a430dbc2fae6c1b28805d82961c8dc
Ariel Flor <arielwb@gmail.com>
2020-08-03 19:42:21 -0300

    chore: fix pre-release distribution channel
    DSY-007

--- bin/release/release.sh
+++ bin/release/release.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-[[ $1 = "pre-release" ]] && PRE_RELEASE="--prerelease $(node ./bin/release/getPreReleaseName.js) --skip.tag --skip.changelog" || PRE_RELEASE=""
+PRE_RELEASE_NAME=$(node ./bin/release/getPreReleaseName.js)
+
+[[ $1 = "pre-release" ]] && PRE_RELEASE="--prerelease ${PRE_RELEASE_NAME} --skip.tag --skip.changelog" || PRE_RELEASE=""
+[[ $PRE_RELEASE = "" ]] && TAG_NAME="latest" || TAG_NAME=$PRE_RELEASE_NAME
 
 if \
   { git log "$( git describe --tags --abbrev=0 )..HEAD" --format='%s' | cut -d: -f1 | sort -u | sed -e 's/([^)]*)//' | grep -q -i -E '^feat|fix|perf|refactor|revert$' ; } || \
@@ -9,7 +12,7 @@ if \
 then
   yarn bump:version $PRE_RELEASE
   echo "//registry.npmjs.org/:_authToken=$NPM_TOKEN" > ~/.npmrc
-  npm publish
+  npm publish --tag $TAG_NAME
   git push --tags
   git push
 else
