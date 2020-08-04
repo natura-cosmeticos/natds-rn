8269a040307f426780a69776fbaf615d2ab23005
Yasminn Vaz <yasminn.vaz@thoughtworks.com>
2020-08-04 18:41:22 -0300

    fix: removes Text import unuseful
    DSY-840

--- src/components/Divider/Divider.stories.tsx
+++ src/components/Divider/Divider.stories.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
 import { Divider } from './Divider';
 import { ContainerWithBorder, TextWithTheme } from '../../common/HelperComponents/ThemeHelper.styles';
 
