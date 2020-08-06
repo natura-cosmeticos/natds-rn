a09f307723a544fa80d246bf921b74dfbb12c4bd
Nicholas Kuchiniski <nicholas@vizir.com.br>
2020-08-06 18:48:39 -0300

    feat(dsy-854-progress-indicator)): create progress indicator component

---
+++ src/components/ProgressIndicator/CircularProgress/CircularProgress.tsx
@@ -0,0 +1,60 @@
+import React, { useEffect } from 'react';
+import {
+  Animated,
+  Easing,
+  Platform,
+} from 'react-native';
+
+import { CircularIndicator } from './CircularIndicator';
+
+export interface CircularProgressProps {
+  size: number;
+  color: string;
+}
+
+export const CircularProgress = ({
+  size,
+  color,
+}: CircularProgressProps) => {
+  /**
+   * Duration specify how much the circle will take to make a 360deg loop around itself,
+   * decrease it will speed up the animation speed and increase will slow the animation speed
+   * The default speed is 1 second per loop
+   */
+  const duration = 1000;
+  /**
+   * This animation/Animated.timing, is responsible for looping the border around the view.
+   */
+  const timer = new Animated.Value(0);
+  const rotation = Animated.timing(timer, {
+    duration,
+    easing: Easing.linear,
+    isInteraction: false,
+    toValue: 1,
+    useNativeDriver: Platform.OS !== 'web',
+  });
+
+  /**
+   * Loop rotation animation continuously,
+   * each time it reaches the end, it resets and begins again from the start.
+   */
+  function startRotation(): void {
+    timer.setValue(0);
+    Animated.loop(rotation).start();
+  }
+
+  /**
+   * Reset the timer and loop the animation again on each update
+   */
+  useEffect(() => {
+    startRotation();
+  });
+
+  return (
+    <CircularIndicator
+      color={color}
+      size={size}
+      timer={timer}
+    />
+  );
+};
---
+++ src/components/ProgressIndicator/ProgressIndicator.device.tsx
@@ -0,0 +1,6 @@
+import { storiesOf } from '@storybook/react-native';
+
+import { circular } from './ProgressIndicator.stories';
+
+storiesOf('ProgressIndicator', module)
+  .add('circular', circular);
