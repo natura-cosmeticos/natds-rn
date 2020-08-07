2c505855d57bad6a157fff6366134469f6447a77
Nicholas Kuchiniski <nicholas@vizir.com.br>
2020-08-06 23:49:41 -0300

    feat(dsy-1296): add label property to radio button

--- src/components/RadioButton/RadioButton.styles.tsx
+++ src/components/RadioButton/RadioButton.styles.tsx
@@ -1,8 +1,20 @@
 
 import styled from 'styled-components/native';
 import {
-  getColorPrimary, Theme, getColorSecondary,
+  getColorPrimary, Theme, getColorSecondary, getColorLowEmphasis, getColorOnBackground,
 } from '../../common/themeSelectors';
+
+export const Container = styled.TouchableOpacity`
+  flex-direction: row;
+  align-items: center;
+`;
+
+export const Label = styled.Text<{ theme: Theme; disabled: boolean }>(({ theme, disabled }) => ({
+  color: disabled ? getColorLowEmphasis(theme) : getColorOnBackground(theme),
+  fontSize: 14,
+  letterSpacing: 0.22,
+  marginLeft: 8,
+}));
 
 export const Circle = styled.View<{
   theme: Theme;
@@ -11,7 +23,7 @@ export const Circle = styled.View<{
 }>(({ theme, color, disabled }) => ({
   alignItems: 'center',
   // eslint-disable-next-line no-nested-ternary
-  borderColor: disabled ? '#777' : color === 'primary' ? getColorPrimary(theme) : getColorSecondary(theme),
+  borderColor: disabled ? getColorLowEmphasis(theme) : color === 'primary' ? getColorPrimary(theme) : getColorSecondary(theme),
   borderRadius: 12,
   borderWidth: '2px',
   height: 20,
@@ -21,7 +33,7 @@ export const Circle = styled.View<{
 
 export const Center = styled.View<{ theme: Theme; color: string }>(({ theme, color }) => ({
   backgroundColor: color === 'primary' ? getColorPrimary(theme) : getColorSecondary(theme),
-  borderRadius: 100,
+  borderRadius: 12,
   height: 10,
   width: 10,
 }));
