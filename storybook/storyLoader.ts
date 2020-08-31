37537a9efc5481b3362f3881334062ff952ba06b
Fellipe Nogueira <fellipe.nogueira@vizir.com.br>
2020-08-31 08:03:36 -0300

    feat: creation of alert component
    DSY-1414

--- storybook/storyLoader.ts
+++ storybook/storyLoader.ts
@@ -4,7 +4,8 @@
 // https://github.com/elderfo/react-native-storybook-loader.git
 
 function loadStories() {
-	require('../src/components/Button/Button.device');
+  require('../src/components/Alert/Alert.device');
+  require('../src/components/Button/Button.device');
 	require('../src/components/Card/Card.device');
 	require('../src/components/Dialog/Dialog.device');
 	require('../src/components/Divider/Divider.device');
@@ -12,11 +13,12 @@ function loadStories() {
 	require('../src/components/IconButton/IconButton.device');
 	require('../src/components/ProgressIndicator/ProgressIndicator.device');
 	require('../src/components/RadioButton/RadioButton.device');
-	require('../src/components/Tab/Tab.device');
+  require('../src/components/Tab/Tab.device');
 	require('../src/tokens/Tokens.device');
 }
 
 const stories = [
+  '../src/components/Alert/Alert.device',
 	'../src/components/Button/Button.device',
 	'../src/components/Card/Card.device',
 	'../src/components/Dialog/Dialog.device',
