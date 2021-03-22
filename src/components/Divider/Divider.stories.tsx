d0b44f002eb427ad3657bde6587882f8004a5edf
Laura Lucca <lauralucca@gmail.com>
2021-03-22 14:15:32 -0300

    docs(divider): add component completion status comparing to ui doc
    DSY-1975

--- src/components/Divider/Divider.stories.tsx
+++ src/components/Divider/Divider.stories.tsx
@@ -3,11 +3,23 @@ import { View } from 'react-native';
 import { Divider } from './Divider';
 import { ContainerWithBorder, TextWithTheme } from '../../common/HelperComponents/ThemeHelper.styles';
 
+const description = () => `
+  ### NOTE:
+  This component is available in the following variants:
+
+  - ✅ Full-bleed
+  - ✅ Inset
+  - ✅ Middle
+`;
+
 export default {
   component: Divider,
   parameters: {
     componentSubtitle:
       'A divider is a thin line that groups content in lists and layouts.',
+    docs: {
+      extractComponentDescription: description,
+    },
   },
   title: 'Components|Divider',
 };
