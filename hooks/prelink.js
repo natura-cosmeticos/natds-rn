473778fb92095de7e1b1f358d83137d30603d398
Ariel Flor <arielwb@gmail.com>
2021-03-26 10:43:36 -0300

    chore: fix link hook
    DSY-000

--- hooks/prelink.js
+++ hooks/prelink.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const fsExtra = require('fs-extra');
 
-const fontFolder = path.join(path.dirname(require.resolve('@naturacosmeticos/natds-icons')), 'fonts');
+const fontFolder = path.join(path.dirname(require.resolve('@naturacosmeticos/natds-icons')), '..', 'fonts');
 
 const destinationFolder = path.join(__dirname, '..', 'src', 'assets', 'fonts');
 
