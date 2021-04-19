00a5547e7577bca1c41679c38162a1fb7f6afb67
Laura Lucca <lauralucca@gmail.com>
2021-04-19 17:26:44 -0300

    docs(icon): remove unwanted stories from sample app
    DSY-1965

--- src/components/Icon/Icon.stories.tsx
+++ src/components/Icon/Icon.stories.tsx
@@ -1,9 +1,6 @@
-/* eslint-disable max-lines */
 import React from 'react';
-import { select } from '@storybook/addon-knobs';
-import { iconNames, IconName } from '@naturacosmeticos/natds-icons';
-import { View } from 'react-native';
-import { ContainerRow } from '../../common/HelperComponents/ThemeHelper.styles';
+import { text as textKnob } from '@storybook/addon-knobs';
+import { IconName } from '@naturacosmeticos/natds-icons';
 import { Icon } from './Icon';
 import { IconColors, IconSizes } from './Icon.types';
 import { StoryContainer } from '../../common/HelperComponents/StoryContainer';
@@ -31,103 +28,18 @@ export default {
   title: 'Components/Icon',
 };
 
-export const All = () => (
-  <View>
-    <Default />
-    <Color />
-    <Name />
-    <Size />
-  </View>
-);
-
 export const Default = () => (
   <StoryContainer title="Default">
     <Icon />
   </StoryContainer>
 );
 
-export const Color = () => (
-  <StoryContainer title="Colors">
-    <Icon color="primary" />
-    <Icon color="secondary" />
-    <Icon color="alert" />
-    <Icon color="link" />
-  </StoryContainer>
-);
-
-export const Name = () => (
-  <StoryContainer title="Names">
-    <Icon name="outlined-social-person" />
-    <Icon name="outlined-product-face" />
-    <Icon name="filled-action-clock" />
-    <Icon name="filled-finance-charging" />
-  </StoryContainer>
-);
-
-export const Size = () => (
-  <StoryContainer title="Sizes">
-    <Icon size="small" />
-    <Icon size="medium" />
-    <Icon size="large" />
-    <Icon size="huge" />
-  </StoryContainer>
-);
-
-/* eslint-disable sort-keys */
-const iconColors = {
-  primary: 'primary',
-  onPrimary: 'onPrimary',
-  primaryLight: 'primaryLight',
-  onPrimaryLight: 'onPrimaryLight',
-  primaryDark: 'primaryDark',
-  onPrimaryDark: 'onPrimaryDark',
-  secondary: 'secondary',
-  onSecondary: 'onSecondary',
-  secondaryLight: 'secondaryLight',
-  onSecondaryLight: 'onSecondaryLight',
-  secondaryDark: 'secondaryDark',
-  onSecondaryDark: 'onSecondaryDark',
-  background: 'background',
-  onBackground: 'onBackground',
-  surface: 'surface',
-  onSurface: 'onSurface',
-  highlight: 'highlight',
-  highEmphasis: 'highEmphasis',
-  mediumEmphasis: 'mediumEmphasis',
-  lowEmphasis: 'lowEmphasis',
-  link: 'link',
-  onLink: 'onLink',
-  success: 'success',
-  onSuccess: 'onSuccess',
-  warning: 'warning',
-  onWarning: 'onWarning',
-  alert: 'alert',
-  onAlert: 'onAlert',
-};
-/* eslint-enable sort-keys */
-
-/* eslint-disable sort-keys */
-const iconSizes = {
-  micro: 'micro',
-  tiny: 'tiny',
-  small: 'small',
-  standard: 'standard',
-  medium: 'medium',
-  large: 'large',
-  largeX: 'largeX',
-  largeXX: 'largeXX',
-  huge: 'huge',
-  hugeX: 'hugeX',
-  hugeXX: 'hugeXX',
-};
-/* eslint-enable sort-keys */
-
 export const Interactive = () => (
-  <ContainerRow style={{ padding: 20 }} >
+  <StoryContainer title="Interactive">
     <Icon
-      color={select('Colors', iconColors, 'highlight') as IconColors}
-      name={select('Icon name', iconNames as Array<IconName>, 'outlined-default-mockup' as IconName)}
-      size={select('Sizes', iconSizes, 'standard') as IconSizes}
+      color={textKnob('Color', 'highlight')as IconColors}
+      name={textKnob('Icon name', 'outlined-default-mockup') as IconName}
+      size={textKnob('Size', 'standard') as IconSizes}
     />
-  </ContainerRow>
+  </StoryContainer>
 );
