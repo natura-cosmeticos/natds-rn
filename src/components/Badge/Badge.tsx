2958f5b3f9a822408bbf46d2503c9ecb96be42c8
Michelly-Oliveira <michellyneris.oliveira@gmail.com>
2021-05-03 12:01:13 -0300

    refactor: testing fix for ios
    DSY-1456

--- src/components/Badge/Badge.tsx
+++ src/components/Badge/Badge.tsx
@@ -3,42 +3,43 @@ import React from 'react';
 import { withTheme } from 'styled-components/native';
 import { Color } from '@naturacosmeticos/natds-themes/react-native';
 
-import {
-  Theme,
-} from '../../common/themeSelectors';
+import { Theme } from '../../common/themeSelectors';
 
 import { BadgeContainer, BadgeContent } from './Badge.style';
 
 export type BadgeTypes = 'dot' | 'standard';
-export type BadgeColors = keyof Pick<Color, 'alert' | 'primary' | 'secondary' | 'success'>;
+export type BadgeColors = keyof Pick<
+  Color,
+  'alert' | 'primary' | 'secondary' | 'success'
+>;
 export type BadgeLimit = 9 | 99 | 'unlimited';
 
 export interface BadgeProps {
   /**
    * Badge color: alert, primary, secondary, success
    * @default: `alert`
-  */
+   */
   color?: BadgeColors;
   /**
    * Used to report the count of something new
-  */
+   */
   content?: number;
   /**
    * Maximum number that can be displayed in the badge
    * @default: `9999`
-  */
+   */
   limit?: BadgeLimit;
   /**
    * ID used for testing
-  */
+   */
   testID?: string;
   /**
    * App's theme
-  */
+   */
   theme: Theme;
   /**
    * Define the style of the badge
-  */
+   */
   type: BadgeTypes;
 }
 
@@ -53,7 +54,7 @@ const BadgeComponent = ({
 
   return (
     <BadgeContainer color={color} type={type} testID={testID}>
-      {content && type === 'standard' && (
+      {content !== undefined && type === 'standard' && (
         <BadgeContent color={color}>
           {useLimit && content > limit ? `${limit}+` : `${content}`}
         </BadgeContent>
