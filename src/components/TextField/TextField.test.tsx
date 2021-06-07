b02f7bba4312e1d402db51594c2171faecedc3c2
Laura Lucca <lauralucca@gmail.com>
2021-06-07 15:16:08 -0300

    chore(textfield): refactor textfield test
    DSY-2081

--- src/components/TextField/TextField.test.tsx
+++ src/components/TextField/TextField.test.tsx
@@ -5,7 +5,6 @@ import { TextField } from './TextField';
 // @ts-ignore
 import placeholderImage from '../../assets/images/anonymous.jpg';
 
-
 jest.mock('../../common/themeSelectors', () => (
   {
     buildColorWithOpacity: () => '#BBBBBB00',
@@ -35,89 +34,92 @@ jest.mock('../../common/themeSelectors', () => (
   }));
 
 describe('Input', () => {
-  it('should render with default props', () => {
-    const { getByTestId } = renderWithTheme(<TextField />);
+  describe('render', () => {
+    it('should render with default props', () => {
+      const { getByTestId } = renderWithTheme(<TextField />);
 
-    const deafultStyles = {
-      backgroundColor: '#000000',
-      borderRadius: 8,
-      color: '#333333',
-      flexGrow: 1,
-      height: 32,
-      overflow: 'hidden',
-      paddingHorizontal: 8,
-    };
+      const defaultStyles = {
+        backgroundColor: '#000000',
+        borderRadius: 8,
+        color: '#333333',
+        flexGrow: 1,
+        height: 32,
+        overflow: 'hidden',
+        paddingHorizontal: 8,
+      };
 
-    expect(getByTestId('input').props.style).toEqual(deafultStyles);
+      expect(getByTestId('input')).toHaveStyle(defaultStyles);
+    });
+    it('should render with given size', () => {
+      const { getByTestId } = renderWithTheme(<TextField size="medium" />);
+
+      expect(getByTestId('input')).toHaveStyle({ height: 24 });
+    });
+    it('should render with correct props when disabled', () => {
+      const { getByTestId } = renderWithTheme(<TextField disabled />);
+
+      expect(getByTestId('input')).toHaveStyle({ color: '#BBBBBB' });
+      expect(getByTestId('input')).toHaveProp('editable', false);
+    });
+    it('should render with correct props when readonly', () => {
+      const { getByTestId } = renderWithTheme(<TextField readonly />);
+
+      expect(getByTestId('input')).toHaveStyle({ backgroundColor: '#BBBBBB00' });
+      expect(getByTestId('input')).toHaveProp('editable', false);
+    });
   });
+  describe('content', () => {
+    it('should get given value', () => {
+      const { getByTestId } = renderWithTheme(<TextField value="testing value" />);
 
-  it('should render with given size', () => {
-    const { getByTestId } = renderWithTheme(<TextField size="medium" />);
+      expect(getByTestId('input')).toHaveProp('value', 'testing value');
+    });
+    it('should get given placeholder', () => {
+      const { getByTestId } = renderWithTheme(<TextField placeholder="testing placeholder" />);
 
-    expect(getByTestId('input').props.style.height).toEqual(24);
+      expect(getByTestId('input')).toHaveProp('placeholder', 'testing placeholder');
+    });
   });
+  describe('status events', () => {
+    it('should call onFocus when focused', () => {
+      const onFocusMock = jest.fn();
+      const { getByTestId } = renderWithTheme(<TextField onFocus={onFocusMock} />);
 
-  it('should render with correct props when disabled', () => {
-    const { getByTestId } = renderWithTheme(<TextField disabled />);
+      fireEvent(getByTestId('input'), 'focus');
 
-    expect(getByTestId('input').props.style.color).toEqual('#BBBBBB');
-    expect(getByTestId('input').props.editable).toEqual(false);
+      expect(onFocusMock).toHaveBeenCalled();
+    });
+    it('should call onBlur when loses focus', () => {
+      const onBlurMock = jest.fn();
+      const { getByTestId } = renderWithTheme(<TextField onBlur={onBlurMock} />);
+
+      fireEvent(getByTestId('input'), 'blur');
+
+      expect(onBlurMock).toHaveBeenCalled();
+    });
   });
+  describe('action', () => {
+    it('should render an icon button when receives action icon', () => {
+      const { getByTestId } = renderWithTheme(
+        <TextField
+          action="icon"
+          actionOnPress={() => {}}
+          iconName="filled-action-clock"
+        />,
+      );
 
-  it('should render with correct props when readonly', () => {
-    const { getByTestId } = renderWithTheme(<TextField readonly />);
+      expect(getByTestId('action-icon')).toBeTruthy();
+    });
+    it('should render an image button when receives action image', () => {
+      const { getByTestId } = renderWithTheme(
+        <TextField
+          action="image"
+          actionOnPress={() => {}}
+          imageSource={placeholderImage}
+        />,
+      );
 
-    expect(getByTestId('input').props.style.backgroundColor).toEqual('#BBBBBB00');
-    expect(getByTestId('input').props.editable).toEqual(false);
-  });
-
-  it('should get content when filled', () => {
-    const { getByTestId } = renderWithTheme(<TextField value="filled" />);
-
-    expect(getByTestId('input').props.value).toEqual('filled');
-  });
-
-  it('should call onFocus when focused', () => {
-    const onFocusMock = jest.fn();
-
-    const { getByTestId } = renderWithTheme(<TextField onFocus={onFocusMock} />);
-
-    fireEvent(getByTestId('input'), 'focus');
-
-    expect(onFocusMock).toHaveBeenCalled();
-  });
-
-  it('should call onBlur when loses focus', () => {
-    const onBlurMock = jest.fn();
-
-    const { getByTestId } = renderWithTheme(<TextField onBlur={onBlurMock} />);
-
-    fireEvent(getByTestId('input'), 'blur');
-
-    expect(onBlurMock).toHaveBeenCalled();
-  });
-
-  it('should render an icon button when receives action icon', () => {
-    const { getByTestId } = renderWithTheme(
-      <TextField
-        action="icon"
-        actionOnPress={() => {}}
-        iconName="filled-action-clock"
-      />,
-    );
-
-    expect(getByTestId('action-icon')).toBeTruthy();
-  });
-
-  it('should render an image button when receives action image', () => {
-    const { getByTestId } = renderWithTheme(
-      <TextField
-        action="image"
-        actionOnPress={() => {}}
-        imageSource={placeholderImage}
-      />,
-    );
-
-    expect(getByTestId('action-image')).toBeTruthy();
+      expect(getByTestId('action-image')).toBeTruthy();
+    });
   });
 });
