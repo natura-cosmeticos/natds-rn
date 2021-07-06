f23a61c4df678e95422d928d13bd0cb165f194e0
Laura Lucca <lauralucca@gmail.com>
2021-07-06 20:51:15 -0300

    chore(deps): update react-native-svg and move to deps
    DSY-1216

--- types/svg.d.ts
+++ types/svg.d.ts
@@ -1,4 +1,7 @@
 declare module '*.svg' {
-  const content: string;
+  import React from 'react';
+  import { SvgProps } from 'react-native-svg';
+
+  const content: React.FC<SvgProps>;
   export default content;
 }
