320d67a378934b5dffbcd2abfb040e6739e32044
mattosdavi <davi.mattos@thoughtworks.com>
2021-09-28 17:32:04 -0300

    feat(typography): creating method to copy fonts by brand
    DSY-2266

---
+++ hooks/copyFontsByBrand.js
@@ -0,0 +1,24 @@
+const path = require('path');
+const fsExtra = require('fs-extra');
+
+const fontsByBrands = {
+  aesop: ['Zapf', 'Suisse'],
+  avon: ['Montserrat'],
+  natura: ['Helvetica'],
+  theBodyShop: ['Druk', 'Recoleta', 'Work'],
+};
+
+module.exports = function copy(brand, destination) {
+  const fontFolder = path.join(path.dirname(require.resolve('@naturacosmeticos/natds-themes')), '..', 'react-native', 'assets');
+  const destinationFolder = path.join(__dirname, '..', destination);
+
+  fontsByBrands[brand].map(fontName => fsExtra.copy(fontFolder, destinationFolder, {
+    filter: (fontPath) => {
+      if (fsExtra.lstatSync(fontPath).isDirectory()) {
+        return true;
+      }
+
+      return fontPath.includes(fontName) && fontPath;
+    },
+  }));
+};
