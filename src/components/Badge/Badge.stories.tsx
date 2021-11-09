c18f8f1a9e34fc9bb9cd4bee4db45bf25406623f
mattosdavi <88327260+mattosdavi@users.noreply.github.com>
2021-11-09 18:18:35 -0300

    feat(badge): create a badge component (#206)
    * feat(badge): create a badge component
    
    DSY-2362

---
+++ src/components/Badge/Badge.stories.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import { Badge } from './Badge';
+import { StoryContainer } from '../../common/HelperComponents/StoryContainer';
+
+const description = () => `
+- - -
+
+  ### NOTE:
+  This component is available in the following variants:
+
+  - ✅ Dot
+  - ✅ Pulse
+  - ✅ Standard
+
+With the following attribute status:
+
+  - **Color:**
+    - ✅ \`Alert\`
+    - ✅ \`Primary\`
+    - ✅ \`Secondary\`
+    - ✅ \`Success\`
+
+  - ✅ **Limit**
+  - ✅ **Value**
+- - -
+`;
+
+export default {
+  component: Badge,
+  parameters: {
+    componentSubtitle: 'Badge is a visual indicator for numeric values.',
+    docs: {
+      extractComponentDescription: description,
+    },
+  },
+  title: 'Components/Badge',
+};
+
+export const All = () => (
+  <StoryContainer title=''>
+    <Badge variant='dot' />
+    <Badge variant='pulse' color='success' />
+    <Badge variant='standard' color='primary' value={10} limit={9} />
+    <Badge variant='standard' color='secondary' value={100} limit={99} />
+    <Badge variant='standard' value={1000} />
+  </StoryContainer>
+);
