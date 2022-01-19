9beb107d5b65ffa1b58eb5b98a2088864acff62c
mattosdavi <davi.mattos@thoughtworks.com>
2022-01-19 15:45:27 -0300

    style(badge): formatting lint rules of commas and semicolon
    DSY-2547

--- src/components/Badge/Badge.styles.ts
+++ src/components/Badge/Badge.styles.ts
@@ -1,49 +1,49 @@
-import styled from 'styled-components/native';
-import { CSSObject } from 'styled-components';
+import styled from 'styled-components/native'
+import { CSSObject } from 'styled-components'
 import {
-  Theme,
-} from '../../common/themeSelectors';
+  Theme
+} from '../../common/themeSelectors'
 
-import { BadgeColors } from './Badge.types';
+import { BadgeColors } from './Badge.types'
 
 type BadgeStyleProps = {
-  color: BadgeColors
-  variant: BadgeVariant
-  theme: Theme
+  color: BadgeColors;
+  variant: BadgeVariant;
+  theme: Theme;
 }
 
 export type BadgeVariant = 'standard' | 'pulse' | 'dot'
-export const convertPulseToDot = (variant: BadgeVariant) => (variant === 'pulse' ? 'dot' : variant);
+export const convertPulseToDot = (variant: BadgeVariant) => (variant === 'pulse' ? 'dot' : variant)
 
 export const getHorizontalPadding = (variant: BadgeVariant, theme: Theme) => (
   variant === 'standard' && {
-    paddingHorizontal: theme.spacing.micro,
+    paddingHorizontal: theme.spacing.micro
   }
-);
+)
 
 export const getWidthByVariant = (variant: BadgeVariant, theme: Theme) => (
   variant !== 'standard' && {
-    width: theme.badge[convertPulseToDot(variant)].height,
+    width: theme.badge[convertPulseToDot(variant)].height
   }
-);
+)
 
 export const Container = styled.View(() => ({
   alignContent: 'center',
-  alignItems: 'center',
-}));
+  alignItems: 'center'
+}))
 
 export const BadgeBase = styled.View(({ theme, color, variant = 'standard' }: BadgeStyleProps): CSSObject => ({
   ...getWidthByVariant(variant, theme),
   backgroundColor: theme.badge.color[color].background,
   borderRadius: theme.badge[convertPulseToDot(variant)].borderRadius,
-  height: theme.badge[convertPulseToDot(variant)].height,
-}));
+  height: theme.badge[convertPulseToDot(variant)].height
+}))
 
 export const Circle = styled(BadgeBase)(({ theme, variant = 'standard' }: BadgeStyleProps): CSSObject => ({
   ...getHorizontalPadding(variant, theme),
   alignContent: 'center',
-  justifyContent: 'center',
-}));
+  justifyContent: 'center'
+}))
 
 export const Label = styled.Text(({ theme, color, variant = 'standard' }: BadgeStyleProps): CSSObject => ({
   color: theme.badge.color[color].label,
@@ -52,5 +52,5 @@ export const Label = styled.Text(({ theme, color, variant = 'standard' }: BadgeS
   fontWeight: theme.badge.label.primary.fontWeight,
   letterSpacing: theme.badge.label.letterSpacing,
   lineHeight: theme.badge[convertPulseToDot(variant)].height,
-  textAlignVertical: 'center',
-}));
+  textAlignVertical: 'center'
+}))
