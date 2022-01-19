b19b331c0e685b7ba5f8a71a997015374a37a6f7
mattosdavi <davi.mattos@thoughtworks.com>
2022-01-19 16:07:56 -0300

    style(radiobutton): formatting lint rules like commas and semicolon
    DSY-2547

--- src/components/RadioButton/RadioButton.stories.tsx
+++ src/components/RadioButton/RadioButton.stories.tsx
@@ -1,19 +1,19 @@
-import React, { useState } from 'react';
-import { View } from 'react-native';
-import { boolean, text } from '@storybook/addon-knobs';
+import React, { useState } from 'react'
+import { View } from 'react-native'
+import { boolean, text } from '@storybook/addon-knobs'
 
-import { RadioButton } from './RadioButton';
+import { RadioButton } from './RadioButton'
 
 export default {
   component: RadioButton,
   parameters: {
-    componentSubtitle: 'Selection controls allow the user to select options.',
+    componentSubtitle: 'Selection controls allow the user to select options.'
   },
-  title: 'Components|RadioButton',
-};
+  title: 'Components|RadioButton'
+}
 
-export const all = () => {
-  const [selected, setSelected] = useState<string | null>(null);
+export const All = () => {
+  const [selected, setSelected] = useState<string | null>(null)
 
   return (
     <View style={{ maxWidth: 600, padding: 30 }}>
@@ -22,23 +22,22 @@ export const all = () => {
         selected={selected === '1'}
         onPress={setSelected}
         label="Primary"
-        value='1'
+        value="1"
       />
       <RadioButton
         label="Disabled"
         disabled
       />
       <RadioButton
-        selected={true}
+        selected
         label="Selected disabled"
         disabled
       />
     </View>
-  );
-};
+  )
+}
 
-
-export const interactive = () => (
+export const Interactive = () => (
   <View style={{ maxWidth: 600, padding: 30 }}>
     <RadioButton
       selected={boolean('Selected', true)}
@@ -48,4 +47,4 @@ export const interactive = () => (
       value={text('Value', 'my-label')}
     />
   </View>
-);
+)
--- src/components/RadioButton/RadioButton.test.tsx
+++ src/components/RadioButton/RadioButton.test.tsx
@@ -1,105 +1,104 @@
-import React from 'react';
-import { ThemeProvider } from 'styled-components/native';
-import { fireEvent, render } from '@testing-library/react-native';
-import renderer from 'react-test-renderer';
-import theme from '../../common/themeSelectors/theme/mock-theme.json';
-import { RadioButton, RadioButtonProps } from './RadioButton';
+import React from 'react'
+import { ThemeProvider } from 'styled-components/native'
+import { fireEvent, render } from '@testing-library/react-native'
+import renderer from 'react-test-renderer'
+import theme from '../../common/themeSelectors/theme/mock-theme.json'
+import { RadioButton, RadioButtonProps } from './RadioButton'
 
 jest.mock(
   'react-native/Libraries/Components/Touchable/TouchableOpacity',
-  () => 'TouchableOpacity',
-);
+  () => 'TouchableOpacity'
+)
 
 const renderRadioButton = (fn, props?: Omit<RadioButtonProps, 'theme'>) => (fn(
   <ThemeProvider theme={theme}>
     <RadioButton {...props} />
-  </ThemeProvider>,
-));
+  </ThemeProvider>
+))
 
 describe('RadioButton component', () => {
   it('should render radio button correctly', () => {
-    const radioButton = renderRadioButton(renderer.create).toJSON();
+    const radioButton = renderRadioButton(renderer.create).toJSON()
 
-    expect(radioButton).toMatchSnapshot();
-  });
+    expect(radioButton).toMatchSnapshot()
+  })
 
   it('should render radio button selected correctly', () => {
-    const radioButton = renderRadioButton(renderer.create, { selected: true }).toJSON();
+    const radioButton = renderRadioButton(renderer.create, { selected: true }).toJSON()
 
-    expect(radioButton).toMatchSnapshot();
-  });
+    expect(radioButton).toMatchSnapshot()
+  })
 
   it('should render radio button with label correctly', () => {
-    const radioButton = renderRadioButton(renderer.create, { label: 'My Label' }).toJSON();
+    const radioButton = renderRadioButton(renderer.create, { label: 'My Label' }).toJSON()
 
-    expect(radioButton).toMatchSnapshot();
-  });
+    expect(radioButton).toMatchSnapshot()
+  })
 
   it('should render radio button selected but disabled correctly', () => {
-    const radioButton = renderRadioButton(renderer.create, { disabled: true }).toJSON();
+    const radioButton = renderRadioButton(renderer.create, { disabled: true }).toJSON()
 
-    expect(radioButton).toMatchSnapshot();
-  });
+    expect(radioButton).toMatchSnapshot()
+  })
 
   it('should render radio button selected but disabled with label correctly', () => {
-    const radioButton = renderRadioButton(renderer.create, { disabled: true, label: 'My Label' }).toJSON();
+    const radioButton = renderRadioButton(renderer.create, { disabled: true, label: 'My Label' }).toJSON()
 
-    expect(radioButton).toMatchSnapshot();
-  });
-
+    expect(radioButton).toMatchSnapshot()
+  })
 
   it('should call the onPress function when the user touches the radio button', () => {
-    const onPress = jest.fn();
+    const onPress = jest.fn()
     const { queryByTestId } = renderRadioButton(render, {
       onPress,
-      value: 'radio-button',
-    });
+      value: 'radio-button'
+    })
 
-    const radioButton = queryByTestId('radio-button');
+    const radioButton = queryByTestId('radio-button')
 
-    fireEvent.press(radioButton);
+    fireEvent.press(radioButton)
 
-    expect(onPress).toHaveBeenCalledWith('radio-button');
-  });
+    expect(onPress).toHaveBeenCalledWith('radio-button')
+  })
 
   it('should call the onPress function when the user touches the radio button label', () => {
-    const onPress = jest.fn();
+    const onPress = jest.fn()
     const { queryByTestId } = renderRadioButton(render, {
       label: 'My label',
       onPress,
-      value: 'radio-button-label',
-    });
+      value: 'radio-button-label'
+    })
 
-    const label = queryByTestId('radio-button-label');
+    const label = queryByTestId('radio-button-label')
 
-    fireEvent.press(label);
+    fireEvent.press(label)
 
-    expect(onPress).toHaveBeenCalledWith('radio-button-label');
-  });
+    expect(onPress).toHaveBeenCalledWith('radio-button-label')
+  })
 
   it('should render circle inside radio when selected is true', () => {
-    const onPress = jest.fn();
+    const onPress = jest.fn()
     const { queryByTestId } = renderRadioButton(render, {
       onPress,
       selected: true,
-      value: 'radio-button',
-    });
+      value: 'radio-button'
+    })
 
-    const radioButtonCircle = queryByTestId('radio-button-circle-selected');
+    const radioButtonCircle = queryByTestId('radio-button-circle-selected')
 
-    expect(radioButtonCircle).not.toBeNull();
-  });
+    expect(radioButtonCircle).not.toBeNull()
+  })
 
   it('should render radio inside label container when label is different from null/undefined', () => {
-    const onPress = jest.fn();
+    const onPress = jest.fn()
     const { queryByTestId } = renderRadioButton(render, {
       label: 'My Label',
       onPress,
-      value: 'radio-button',
-    });
+      value: 'radio-button'
+    })
 
-    const radioButtonLabel = queryByTestId('radio-button-label');
+    const radioButtonLabel = queryByTestId('radio-button-label')
 
-    expect(radioButtonLabel).not.toBeNull();
-  });
-});
+    expect(radioButtonLabel).not.toBeNull()
+  })
+})
