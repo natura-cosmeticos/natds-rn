4b18df4e6dd6e28cb0365be48bb840433ba170ba
mattosdavi <88327260+mattosdavi@users.noreply.github.com>
2022-02-03 14:48:13 -0300

    feat(counter): adds border radius token (#242)
    * feat(counter): adds border radius token
    
    DSY-2552
    
    * chore(deps): update natds themes dependency
    
    DSY-2552
    
    Co-authored-by: @mattosdavi

--- src/components/Counter/Counter.device.tsx
+++ src/components/Counter/Counter.device.tsx
@@ -1,19 +1,17 @@
 import { storiesOf } from '@storybook/react-native'
 import {
-  All,
   Default,
   Disabled,
   Interactive,
   Label,
   Size,
-  Value
+  Range
 } from './Counter.stories'
 
 storiesOf('Counter', module)
-  .add('All', All)
   .add('Default', Default)
   .add('Label', Label)
   .add('Size', Size)
   .add('Disabled', Disabled)
-  .add('Value', Value)
+  .add('Range', Range)
   .add('Interactive', Interactive)
