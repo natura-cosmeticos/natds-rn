2015d73d5846ab7f0c02d0e71e97dae9b52be115
mattosdavi <davi.mattos@thoughtworks.com>
2022-04-18 14:31:27 -0300

    chore(hooks): revert prelink method refactor won't work on android os
    DSY-2665

--- hooks/prelink.js
+++ hooks/prelink.js
@@ -1,22 +1,7 @@
-const fs = require('fs')
 const path = require('path')
+const fsExtra = require('fs-extra')
 
 const fontFolder = path.join(path.dirname(require.resolve('@naturacosmeticos/natds-icons')), '..', 'fonts')
 const destinationFolder = path.join(__dirname, '..', 'src', 'assets', 'fonts')
 
-function recursiveCopySync(source, target) {
-  if (fs.lstatSync(source).isDirectory()) {
-    if (!fs.existsSync(target)) {
-      fs.mkdirSync(target)
-    }
-    const files = fs.readdirSync(source)
-    files.forEach((file) => {
-      recursiveCopySync(path.join(source, file), path.join(target, file))
-    })
-  } else if (fs.existsSync(source)) {
-    fs.writeFileSync(target, fs.readFileSync(source))
-  }
-  console.log(`Copied ${source} to ${target}`)
-}
-
-recursiveCopySync(fontFolder, destinationFolder)
+fsExtra.copy(fontFolder, destinationFolder)
