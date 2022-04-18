3f2c6c14464d616ab4e8365bb66c8db4ba9c4bcf
mattosdavi <davi.mattos@thoughtworks.com>
2022-04-18 12:15:45 -0300

    fix(hooks): fix method to link icon fonts
    DSY-2665

--- hooks/prelink.js
+++ hooks/prelink.js
@@ -1,8 +1,22 @@
-const path = require('path');
-const fsExtra = require('fs-extra');
+const fs = require('fs')
+const path = require('path')
 
-const fontFolder = path.join(path.dirname(require.resolve('@naturacosmeticos/natds-icons')), '..', 'fonts');
+const fontFolder = path.join(path.dirname(require.resolve('@naturacosmeticos/natds-icons')), '..', 'fonts')
+const destinationFolder = path.join(__dirname, '..', 'src', 'assets', 'fonts')
 
-const destinationFolder = path.join(__dirname, '..', 'src', 'assets', 'fonts');
+function recursiveCopySync(source, target) {
+  if (fs.lstatSync(source).isDirectory()) {
+    if (!fs.existsSync(target)) {
+      fs.mkdirSync(target)
+    }
+    const files = fs.readdirSync(source)
+    files.forEach((file) => {
+      recursiveCopySync(path.join(source, file), path.join(target, file))
+    })
+  } else if (fs.existsSync(source)) {
+    fs.writeFileSync(target, fs.readFileSync(source))
+  }
+  console.log(`Copied ${source} to ${target}`)
+}
 
-fsExtra.copy(fontFolder, destinationFolder);
+recursiveCopySync(fontFolder, destinationFolder)
