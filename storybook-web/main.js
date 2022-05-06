fd5ff842ee2f1ec2fe920a7a2a3e8fff8b8bc2f7
mattosdavi <davi.mattos@thoughtworks.com>
2022-05-06 18:13:45 -0300

    refactor(storybook): apply others svg types in webpack config
    DSY-1185

--- storybook-web/main.js
+++ storybook-web/main.js
@@ -1,28 +1,28 @@
 module.exports = {
   presets: ['@storybook/addon-docs/preset'],
   webpackFinal: (config) => {
-    const newConfig = { ...config };
+    const newConfig = { ...config }
 
     newConfig.resolve.alias = {
       ...(config.resolve.alias || {}),
       '../../common/Modal': 'modal-react-native-web',
       '@storybook/react-native': '@storybook/react',
       'react-native$': 'react-native-web',
-      'react-native-svg': './Logo.web',
-    };
+      'react-native-svg': './Storybook.web'
+    }
 
-    newConfig.module.rules[0].use[0].options.plugins.push(['@babel/plugin-proposal-optional-chaining']);
-    newConfig.module.rules[0].use[0].options.plugins.push(['react-native-web', { commonjs: true }]);
+    newConfig.module.rules[0].use[0].options.plugins.push(['@babel/plugin-proposal-optional-chaining'])
+    newConfig.module.rules[0].use[0].options.plugins.push(['react-native-web', { commonjs: true }])
 
     newConfig.module.rules[7] = {
       test: /\.(gif|jpe?g|png|svg)$/,
       use: {
         loader: 'url-loader',
         options: {
-          name: '[name].[ext]',
-        },
-      },
-    };
+          name: '[name].[ext]'
+        }
+      }
+    }
 
     newConfig.module.rules.push({
       test: /\.(ts|tsx)$/,
@@ -31,19 +31,19 @@ module.exports = {
           loader: require.resolve('ts-loader'),
           options: {
             configFile: require.resolve('./tsconfig.web.json'),
-            transpileOnly: true,
-          },
+            transpileOnly: true
+          }
         },
         {
-          loader: require.resolve('react-docgen-typescript-loader'),
-        },
-      ],
-    });
+          loader: require.resolve('react-docgen-typescript-loader')
+        }
+      ]
+    })
 
-    newConfig.resolve.extensions.push('.ts', '.tsx', '.mdx', '.web.js');
+    newConfig.resolve.extensions.push('.ts', '.tsx', '.mdx', '.web.js')
 
     // console.dir(newConfig, { depth: null });
 
-    return newConfig;
-  },
-};
+    return newConfig
+  }
+}
