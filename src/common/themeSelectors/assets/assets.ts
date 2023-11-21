77ac2354a26354cabd751a2fc0af58080ea4982e
vagnerbarbosant <vagnerbarbosa.foursys@natura.net>
2023-11-21 11:39:21 -0300

    test(natds-rn): update snapshot mock-theme affects: @naturacosmeticos/natds-rn DSY-3815-v2

--- src/common/themeSelectors/assets/assets.ts
+++ src/common/themeSelectors/assets/assets.ts
@@ -12,22 +12,30 @@ const getBrandAssetsByModel = (
 ) => getBrandAssets(theme)[type][model]
 
 export const getNeutralLogoA = (theme: Theme) => ({
+  es: getBrandAssetsByModel(theme, 'neutral', 'a').es,
   file: getBrandAssetsByModel(theme, 'neutral', 'a').file,
   height: getBrandAssetsByModel(theme, 'neutral', 'a').height,
+  pt: getBrandAssetsByModel(theme, 'neutral', 'a').pt,
   width: getBrandAssetsByModel(theme, 'neutral', 'a').width
 })
 export const getNeutralLogoB = (theme: Theme) => ({
+  es: getBrandAssetsByModel(theme, 'neutral', 'b').es,
   file: getBrandAssetsByModel(theme, 'neutral', 'b').file,
   height: getBrandAssetsByModel(theme, 'neutral', 'b').height,
+  pt: getBrandAssetsByModel(theme, 'neutral', 'b').pt,
   width: getBrandAssetsByModel(theme, 'neutral', 'b').width
 })
 export const getCustomLogoA = (theme: Theme) => ({
+  es: getBrandAssetsByModel(theme, 'custom', 'a').es,
   file: getBrandAssetsByModel(theme, 'custom', 'a').file,
   height: getBrandAssetsByModel(theme, 'custom', 'a').height,
+  pt: getBrandAssetsByModel(theme, 'custom', 'a').pt,
   width: getBrandAssetsByModel(theme, 'custom', 'a').width
 })
 export const getCustomLogoB = (theme: Theme) => ({
+  es: getBrandAssetsByModel(theme, 'custom', 'b').es,
   file: getBrandAssetsByModel(theme, 'custom', 'b').file,
   height: getBrandAssetsByModel(theme, 'custom', 'b').height,
+  pt: getBrandAssetsByModel(theme, 'custom', 'b').pt,
   width: getBrandAssetsByModel(theme, 'custom', 'b').width
 })
