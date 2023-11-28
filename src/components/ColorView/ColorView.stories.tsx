f84245c35d54255e08b984f2feeb92464b042961
vagnerbarbosant <vagnerbarbosa.foursys@natura.net>
2023-11-28 19:04:37 -0300

    feat(natds-rn): add ColorView Component affects: @naturacosmeticos/natds-rn DSY-3815-v2

---
+++ src/components/ColorView/ColorView.stories.tsx
@@ -0,0 +1,71 @@
+/* eslint-disable no-restricted-syntax */
+import React from 'react'
+import { Color } from '@naturacosmeticos/natds-themes/react-native'
+import ColorView from './ColorView'
+import { buildTheme } from '../../common/themeSelectors'
+
+const description = () => `
+> Colors are used to identify a user.
+
+## Properties
+| Property                   | Values                                                                                            |    Status           |
+|---                         |                                                                                                ---|                  ---|
+| **size**                   | standard, medium, semiX, tiny, small, semi, large, largeXXX, huge                                 | ✅ Available        |
+| **accessibilityLabel**     | string                                                                                            | ✅ Available        |
+| **accessibilityHint**      | string                                                                                            | ✅ Available        |
+| **type**                   | image, icon, letter, anonymous                                                                    | ✅ Available        |
+| **imgSource**              | string, uri: string   (available only in image type)                                              | ✅ Available        |
+| **text**                   | string, (available only in letter type)                                                           | ✅ Available        |
+| **iconName**               | icon-name, (available only in icon type)                                                          | ✅ Available        |
+| **brand**                  | avon, avon_v2, natura, natura_v2, theBodyShop, <br /> consultoriaDeBeleza, casaEestilo            | ✅ Available        |
+
+
+## Technical Usages Examples
+`
+
+export default {
+  component: <>Colors</>,
+  parameters: {
+    componentSubtitle: '',
+    docs: {
+      extractComponentDescription: description
+    }
+  },
+  title: 'Components/ColorView'
+}
+type PropsKey = {
+  keys: string[];
+}
+
+const keysToRemove = ['neutral50',
+  'neutral100', 'neutral200',
+  'neutral300', 'neutral400',
+  'neutral500', 'neutral600',
+  'neutral700', 'neutral800',
+  'neutral900'
+]
+function removeKeys(obj: Color) {
+  for (const key of keysToRemove) {
+    if (obj[key]) {
+      // eslint-disable-next-line no-param-reassign
+      delete obj[key]
+    }
+  }
+  return obj
+}
+
+export const ColorViews = () => {
+  const theme = buildTheme('natura', 'light')
+  const { color } = theme
+  const ArrayTheme = Object.entries(removeKeys(color))
+
+  return (
+
+    <div style={{ display: 'flex', flexWrap: 'wrap', padding: '30px' }}>
+      {
+        ArrayTheme.map((label) => <ColorView labelToken={label[0]} keyToken={label[1]} />)
+
+      }
+    </div>
+  )
+}
---
+++ src/components/ColorView/ColorView.styles.ts
@@ -0,0 +1,34 @@
+import styled from 'styled-components/native'
+import { Color, Theme } from '@naturacosmeticos/natds-themes/react-native'
+import { CSSObject } from 'styled-components'
+
+export type ColorViewColors = keyof Color
+type PropsColorView = {
+  color: ColorViewColors;
+  theme: Theme;
+}
+
+export const Container = styled.View(({ theme, color = 'primary' }: PropsColorView): CSSObject => ({
+  display: 'flex',
+  width: '240px',
+  height: '40px',
+  padding: '5px',
+  alignContent: 'center',
+  justifyContent: 'center',
+  border: '1px solid #ccc',
+  backgroundColor: theme.color[color],
+  gap: '5px'
+}))
+export const Grid = styled.View(({ theme, color = 'primary' }: PropsColorView): CSSObject => ({
+  display: 'flex',
+  width: '320px',
+  height: '60px',
+  padding: '5px',
+  alignContent: 'center',
+  justifyContent: 'center',
+  backgroundColor: theme.color[color],
+  gap: '5px'
+}))
+export const ColorText = styled.View(({ theme }: PropsColorView): CSSObject => ({
+  color: theme.color.lowEmphasis
+}))
