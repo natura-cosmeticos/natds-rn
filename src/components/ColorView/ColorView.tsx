5824256bdc1ea0631fb271d8ef16524769c72915
vagnerbarbosant <vagnerbarbosa.foursys@natura.net>
2025-01-07 06:46:44 -0300

    fix(natds-rn):  fixed colorView Stories affects: @naturacosmeticos/natds-rn DSY-5412

--- src/components/ColorView/ColorView.tsx
+++ src/components/ColorView/ColorView.tsx
@@ -15,13 +15,17 @@ const ColorView = ({
   labelToken, keyToken, deprecated, internalOnly
 }: tokenLab) => (
 
-  <div style={{
-    display: 'flex', flexDirection: 'column', gap: '5px', padding: '5px', marginBottom: '15px'
-  }}
-  >
-    <div style={{
-      display: 'flex', flexDirection: 'column', width: '240px', justifyContent: 'space-between'
+  <div
+    key={`${keyToken}`}
+    style={{
+      display: 'flex', flexDirection: 'column', gap: '5px', padding: '5px', marginBottom: '15px'
     }}
+  >
+    <div
+      key={`${labelToken}`}
+      style={{
+        display: 'flex', flexDirection: 'column', width: '240px', justifyContent: 'space-between'
+      }}
     >
       <Typography variant="subtitle2">{labelToken}</Typography>
       <Typography variant="body2"><ColorText>{keyToken}</ColorText></Typography>
